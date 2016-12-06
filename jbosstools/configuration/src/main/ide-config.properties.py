6c109c1a87be141921c5f19e4ba417e7bdc09c51
nickboldt <nboldt@redhat.com>
2016-12-06 15:41:54 -0500

    JBIDE-23475 ensure that file handles are closed; ensure that fragment files are updated when regex changes are applied
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- jbosstools/configuration/src/main/ide-config.properties.py
+++ jbosstools/configuration/src/main/ide-config.properties.py
@@ -2,6 +2,8 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 from optparse import OptionParser
 import re
+from os import remove
+from shutil import move
 
 usage = "\n\n\
 Usage 1: %prog -i <version_jbt> -d <version_ds> -t <buildType> -O </path/to/outputFile>\n\n\
@@ -29,21 +31,37 @@ filenames = ['ide-config.current-snapshots-fragment.properties', 'ide-config.cur
 
 d = {'snapshots': options.snapshotsRegex, 'staging': options.stagingRegex, 'development': options.developmentRegex, 'stable': options.stableRegex} 
 
+# merge regex changes into .merged files
+for fname in filenames:
+    with open(dir_path + os.sep + fname) as infile:
+        outfile = open(dir_path + os.sep + fname + '.merged', 'w')
+        for line in infile:
+            linePrinted = 0 # have we printed the file?
+            fileType=re.sub('ide-config.current-(.+)-fragment.properties', r"\1", fname) # snapshots
+            for fileTypeString, regexGroups in d.items():
+                if fileType == fileTypeString and regexGroups: # if we're doing the snapshots file and there's a snapshots regex defined
+                    regexpairs = regexGroups.split(";") # 4.4.2.Final,4.4.3.AM1 and 10.2.0.GA,10.3.0.AM1
+                    lineFixed = line
+                    for regexpair in regexpairs:
+                        regex = regexpair.split(",") # 4.4.2.Final -> 4.4.3.AM1
+                        lineFixed = re.sub(regex[0], regex[1], lineFixed)
+                    outfile.write(lineFixed)
+                    linePrinted = 1
+            if linePrinted == 0:
+                outfile.write(line)
+        outfile.close()
+    infile.close()
+
 with open(outputFile, 'w') as outfile:
     for fname in filenames:
+        # rename the .merged files to overwrite their original file
+        if os.path.exists(dir_path + os.sep + fname + '.merged'):
+            remove(dir_path + os.sep + fname)
+            move(dir_path + os.sep + fname + '.merged', dir_path + os.sep + fname)
+        # merge fragments into ide-config.properties
         with open(dir_path + os.sep + fname) as infile:
             for line in infile:
-                linePrinted = 0 # have we printed the file?
-                fileType=re.sub('ide-config.current-(.+)-fragment.properties', r"\1", fname) # snapshots
-                for fileTypeString, regexGroups in d.items():
-                    if fileType == fileTypeString and regexGroups: # if we're doing the snapshots file and there's a snapshots regex defined
-                        regexpairs = regexGroups.split(";") # 4.4.2.Final,4.4.3.AM1 and 10.2.0.GA,10.3.0.AM1
-                        lineFixed = line
-                        for regexpair in regexpairs:
-                            regex = regexpair.split(",") # 4.4.2.Final -> 4.4.3.AM1
-                            lineFixed = re.sub(regex[0], regex[1], lineFixed)
-                        outfile.write(lineFixed)
-                        linePrinted = 1
-                if linePrinted == 0:
-                    outfile.write(line)
+                outfile.write(line)
+        infile.close()
+outfile.close()
 
